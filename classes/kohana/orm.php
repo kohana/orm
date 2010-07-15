42ad9dbcdba6a19d5959d2878395571a4848881f
John Heathco <jheathco@gmail.com>
2010-07-15 11:21:34 -0700

    Fixing #2803

--- classes/kohana/orm.php
+++ classes/kohana/orm.php
@@ -696,28 +696,7 @@ class Kohana_ORM {
 
 			$this->_db_applied[$name] = $name;
 
-			switch (count($args))
-			{
-				case 0:
-					$this->_db_builder->$name();
-				break;
-				case 1:
-					$this->_db_builder->$name($args[0]);
-				break;
-				case 2:
-					$this->_db_builder->$name($args[0], $args[1]);
-				break;
-				case 3:
-					$this->_db_builder->$name($args[0], $args[1], $args[2]);
-				break;
-				case 4:
-					$this->_db_builder->$name($args[0], $args[1], $args[2], $args[3]);
-				break;
-				default:
-					// Here comes the snail...
-					call_user_func_array(array($this->_db_builder, $name), $args);
-				break;
-			}
+			call_user_func_array(array($this->_db_builder, $name), $args);
 		}
 
 		return $this;
