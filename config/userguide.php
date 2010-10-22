ccecc7eea4fac70cd647a950de0cd046ea47a338
bluehawk <phroughy@gmail.com>
2010-10-21 20:13:33 -0600

    Updated userguide description

--- config/userguide.php
+++ config/userguide.php
@@ -14,7 +14,7 @@ return array(
 			'name' => 'ORM',
 
 			// A short description of this module, shown on the index page
-			'description' => 'Official ORM module, a modeling library for object relational mapping.',
+			'description' => 'Object modeling and relation mapping.',
 			
 			// Copyright message, shown in the footer for this module
 			'copyright' => '&copy; 2008–2010 Kohana Team',
