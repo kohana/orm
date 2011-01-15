3e874f781055ad0f16bf14182b5cb06bbf6043be
Kiall Mac Innes <kiall@managedit.ie>
2011-01-15 21:40:56 +0000

    Fixes #3607 - Adding create_user() and update_user() methods to ORM's Auth_User_Model.

--- classes/model/auth/user.php
+++ classes/model/auth/user.php
@@ -165,4 +165,76 @@ class Model_Auth_User extends ORM {
 		return Valid::email($value) ? 'email' : 'username';
 	}
 
+	/**
+	 * Password validation for plain passwords.
+	 *
+	 * @param array $values
+	 * @return Validation
+	 */
+	public static function get_password_validation($values)
+	{
+		return Validation::factory($values)
+			->rule('password', 'min_length', array(':value', 8))
+			->rule('password_confirm', 'matches', array(':validation', ':field', 'password'));
+	}
+
+	/**
+	 * Create a new user
+	 *
+	 * Example usage:
+	 * ~~~
+	 * $user = ORM::factory('user')->create_user($_POST, array(
+	 *	'username',
+	 *	'password',
+	 *	'email',
+	 * );
+	 * ~~~
+	 *
+	 * @param array $values
+	 * @param array $expected
+	 * @throws ORM_Validation_Exception
+	 */
+	public function create_user($values, $expected)
+	{
+		// Validation for passwords
+		$extra_validation = Model_User::get_password_validation($values)
+			->rule('password', 'not_empty');
+
+		return $this->values($values, $expected)->create($extra_validation);
+	}
+
+	/**
+	 * Update an existing user
+	 *
+	 * [!!] We make the assumption that if a user does not supply a password, that they do not wish to update their password.
+	 *
+	 * Example usage:
+	 * ~~~
+	 * $user = ORM::factory('user')
+	 *	->where('username', '=', 'kiall')
+	 *	->find()
+	 *	->update_user($_POST, array(
+	 *		'username',
+	 *		'password',
+	 *		'email',
+	 *	);
+	 * ~~~
+	 *
+	 * @param array $values
+	 * @param array $expected
+	 * @throws ORM_Validation_Exception
+	 */
+	public function update_user($values, $expected = NULL)
+	{
+		if (empty($values['password']))
+		{
+			unset($values['password'], $values['password_confirm']);
+		}
+
+		// Validation for passwords
+		$extra_validation = Model_User::get_password_validation($values);
+
+		return $this->values($values, $expected)->update($extra_validation);
+	}
+
 } // End Auth User Model
