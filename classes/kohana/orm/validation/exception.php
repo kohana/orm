efd9cd3628b4d5f3360aef5ba7b323cd808b8bae
Kiall Mac Innes <kiall@managedit.ie>
2011-09-20 02:46:12 +0100

    Temporary fix for _external validation messages - see #4185 and #3877
    Refs #4185 and Refs #3877

--- classes/kohana/orm/validation/exception.php
+++ classes/kohana/orm/validation/exception.php
@@ -150,7 +150,15 @@ class Kohana_ORM_Validation_Exception extends Kohana_Exception {
 		{
 			if (is_array($object))
 			{
-				$errors[$key] = $this->generate_errors($alias.DIRECTORY_SEPARATOR.$key, $object, $directory, $translate);
+				// Temporary hack - see #4185 and #3877
+				if ($key == '_external')
+				{
+					$errors[$key] = $this->generate_errors($alias.DIRECTORY_SEPARATOR.$key, $object, $directory, $translate);
+				}
+				else
+				{
+					$errors[$key] = $this->generate_errors($key, $object, $directory, $translate);
+				}
 			}
 			elseif ($object instanceof Validation)
 			{
