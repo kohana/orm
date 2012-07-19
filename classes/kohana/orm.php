02b7edab26f7c80e664336f1baae8f25acffc7d2
Rob Taylor <robt@hoopassociates.co.uk>
2012-07-19 15:48:50 +0100

    Replace array_key_exists() with faster isset() in Kohana_ORM::_load_values()
    See: http://juliusbeckmann.de/blog/php-benchmark-isset-or-array_key_exists.html

--- classes/kohana/orm.php
+++ classes/kohana/orm.php
@@ -997,21 +997,18 @@ class Kohana_ORM extends Model implements serializable {
 	 */
 	protected function _load_values(array $values)
 	{
-		if (array_key_exists($this->_primary_key, $values))
+		if (isset($values[$this->_primary_key]))
 		{
-			if ($values[$this->_primary_key] !== NULL)
-			{
-				// Flag as loaded and valid
-				$this->_loaded = $this->_valid = TRUE;
+            // Flag as loaded and valid
+            $this->_loaded = $this->_valid = TRUE;
 
-				// Store primary key
-				$this->_primary_key_value = $values[$this->_primary_key];
-			}
-			else
-			{
-				// Not loaded or valid
-				$this->_loaded = $this->_valid = FALSE;
-			}
+            // Store primary key
+            $this->_primary_key_value = $values[$this->_primary_key];
+		}
+		else
+		{
+            // Not loaded or valid
+            $this->_loaded = $this->_valid = FALSE;
 		}
 
 		// Related objects
