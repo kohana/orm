c8fbce8fb3d50dedc9359668089a73a639fdc388
Sascha Galley <sg@seso.at>
2014-02-21 14:29:57 +0100

    Updated comments: role -> Role

--- classes/Kohana/ORM.php
+++ classes/Kohana/ORM.php
@@ -1442,7 +1442,7 @@ class Kohana_ORM extends Model implements serializable {
 	 * 
 	 *
 	 *     // Check if $model has the login role
-	 *     $model->has('roles', ORM::factory('role', array('name' => 'login')));
+	 *     $model->has('roles', ORM::factory('Role', array('name' => 'login')));
 	 *     // Check for the login role if you know the roles.id is 5
 	 *     $model->has('roles', 5);
 	 *     // Check for all of the following roles
@@ -1484,7 +1484,7 @@ class Kohana_ORM extends Model implements serializable {
 	 * only checks that at least one of the relationships is satisfied.
 	 *
 	 *     // Check if $model has the login role
-	 *     $model->has('roles', ORM::factory('role', array('name' => 'login')));
+	 *     $model->has('roles', ORM::factory('Role', array('name' => 'login')));
 	 *     // Check for the login role if you know the roles.id is 5
 	 *     $model->has('roles', 5);
 	 *     // Check for any of the following roles
@@ -1505,7 +1505,7 @@ class Kohana_ORM extends Model implements serializable {
 	 * Returns the number of relationships 
 	 *
 	 *     // Counts the number of times the login role is attached to $model
-	 *     $model->count_relations('roles', ORM::factory('role', array('name' => 'login')));
+	 *     $model->count_relations('roles', ORM::factory('Role', array('name' => 'login')));
 	 *     // Counts the number of times role 5 is attached to $model
 	 *     $model->count_relations('roles', 5);
 	 *     // Counts the number of times any of roles 1, 2, 3, or 4 are attached to
